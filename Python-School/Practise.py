3bea25438175933d42ed178da9786d9449c06f21
jb26444 <jan.blahuta@gmail.com>
2018-11-24 17:20:10 +0100

    UBUNTU

---
+++ Python-School/Practise.py
@@ -0,0 +1,33 @@
+
+
+#Python list
+
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"];
+
+my_list = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+
+# print list
+print(my_list)
+
+#print value from the list
+
+a=my_list[1]
+
+print(a)
+
+b=my_list[-1]
+
+print(b)
+
+c=my_list
+del c[1]
+
+print(c)
+
+
+
+
+
+
