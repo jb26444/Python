f7d937e68f74d77fbb6a358b4afa1dec6bedc983
jb26444 <jan.blahuta@gmail.com>
2018-11-24 21:51:33 +0100

    Ubuntu

--- Python-School/service_instance.py
+++ Python-School/service_instance.py
@@ -1,11 +1,25 @@
-input_string = input("Enter a list element separated by space ")
+
+input_string = input("Enter a list of service instances separated by space ")
 list  = input_string.split()
 print(list)
 
-print("!")
 
-for p in list:
+control = input("Is the list above correct? Y/N: ")
+
+
+if control == "Y":
+  print("!")
+  for p in list:
     print("service instance " + p + " ethernet")
     print("encapsulation dot1q " + p)
     print("bridge-domain " + p)
     print("!")
+elif control == "y":
+  print("!")
+  for p in list:
+    print("service instance " + p + " ethernet")
+    print("encapsulation dot1q " + p)
+    print("bridge-domain " + p)
+    print("!")
+else:
+    print("Start again then :)")
